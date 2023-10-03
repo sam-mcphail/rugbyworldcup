ef1231bcacd6e1a2dfe8c627a0d2f7f778e4b8bf
Sam McPhail <sam.mcphail@hotmail.co.nz>
2023-10-03 14:35:49 +1300

    Change from array of objects to individual objects for teams and add classes for all table elements

--- worldcup.js
+++ worldcup.js
@@ -1,216 +1,223 @@
 // Pool A Teams //
-poolATeam = [
-  {
-    name: 'France',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'New Zealand',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Italy',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Uruguay',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Namibia',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-]
+let france = {
+  name: 'France',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let newZealand = {
+  name: 'New Zealand',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let italy = {
+  name: 'Italy',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let uruguay = {
+  name: 'Uruguay',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let namibia = {
+  name: 'Namibia',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
 
 // Pool B Teams //
+let southAfrica = {
+  name: 'South Africa',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
 
-poolBTeam = [
-  {
-    name: 'South Africa',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Ireland',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Scotland',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Tonga',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Romania',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-]
+let ireland = {
+  name: 'Ireland',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let scotland = {
+  name: 'Scotland',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let tonga = {
+  name: 'Tonga',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let romania = {
+  name: 'Romania',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
 
 // Pool C Teams //
-poolCTeam = [
-  {
-    name: 'Wales',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Australia',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Fiji',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Georgia',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Portugal',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-]
+let wales = {
+  name: 'Wales',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let australia = {
+  name: 'Australia',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let fiji = {
+  name: 'Fiji',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let georgia = {
+  name: 'Georgia',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let portugal = {
+  name: 'Portugal',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
 
 // Pool D Teams //
-poolATeam = [
-  {
-    name: 'England',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Argentina',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Japan',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Samoa',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-  {
-    name: 'Chile',
-    played: 0,
-    won: 0,
-    drawn: 0,
-    lost: 0,
-    pointsDif: 0,
-    bonusPoints: 0,
-    points: 0,
-  },
-]
+let england = {
+  name: 'England',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let argentina = {
+  name: 'Argentina',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let japan = {
+  name: 'Japan',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let samoa = {
+  name: 'Samoa',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
+
+let chile = {
+  name: 'Chile',
+  played: 0,
+  won: 0,
+  drawn: 0,
+  lost: 0,
+  pointsDif: 0,
+  bonusPoints: 0,
+  points: 0,
+}
